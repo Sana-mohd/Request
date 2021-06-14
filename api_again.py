67400d21d71f614e748e5337cdd740223417c5ed
sana <mohammedsana20@navgurukul.org>
2021-06-14 16:20:34 +0530

    first commit

--- api_again.py
+++ api_again.py
@@ -1,14 +1,11 @@
 import requests
 import json
-
 course=[]
 id_list=[]
 slug_list=[]
-
 def api_first():
     x = requests.get('http://saral.navgurukul.org/api/courses')
     y=(x.text)
-    # print(x.text)
     with open("courses.json","w") as f:
         fi=json.loads(y)
         json.dump(fi,f,indent=2)
@@ -21,7 +18,6 @@ def api_first():
         id_list.append(id_avaible)
         print(index,course_available,id_avaible)
         index+=1
-    # global user
     user=int(input("Enter id:- "))
     global w
     t=list1[user]
@@ -29,14 +25,11 @@ def api_first():
     print("****","Cource Name : ",t["name"],"****")
     print()
 api_first()
-
 def api_second():
     s="http://saral.navgurukul.org/api/courses/74/exercises"
     s=s.replace("74",w)
     a=requests.get(s)
-    # print(a)
     b=(a.text)
-
     with open("data2.json","w") as file1:
         file2=json.loads(b)
         json.dump(file2,file1,indent=4)
@@ -51,23 +44,21 @@ def api_second():
         if len(u["childExercises"])==0:
             print("       ",u["childExercises"],"       ")
         else:
-            # print("childExercise")
             child_dict=u["childExercises"]
             ind=0
             while ind<len(child_dict):
-                # print(type(child_dict[ind]))
                 for  kill in child_dict[ind]:
                     if kill=="name":
                         sub+=1
                         print("       " ,child_dict[ind][kill],"       ")
                 ind+=1
-    print(len(slug_list))
+    print(len(slug_list),slug_list)
     file1.close()
 api_second()
-
 def slug_fun():
     user_up_slug=input("enter weather you want to do up or slug: ")
     if user_up_slug=="up":
+        slug_list.clear()
         api_first()
         api_second()
         slug_fun()
@@ -75,14 +66,12 @@ def slug_fun():
         user_slug=int(input("enter your slug index: "))
         url='http://saral.navgurukul.org/api/courses/75/exercise/getBySlug?slug=requests__using-json'
         url=url.replace('requests__using-json',slug_list[user_slug])
-    
         slug_url= requests.get(url)
         print(slug_url.text)
         next_previous=input("enter weather you want 1. up 2. Next 3. Previous 4. exit ")
         leng=len(slug_list)
-        # print(slug_url)
-        # print(slug_list[user_slug+1])
         if next_previous=="up":
+            slug_list.clear()
             api_first()
             api_second()
             slug_fun()
